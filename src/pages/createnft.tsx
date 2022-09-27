b073d1ecaf677ec0fcb63f52f944fdb3a8b3ba48
0xRajkumar <rajkumarcoolx@gmail.com>
2022-09-27 19:11:12 +0530

    ipfs-http-client -> web3.storage

--- src/pages/createnft.tsx
+++ src/pages/createnft.tsx
@@ -31,8 +31,11 @@ import { Textarea } from "@chakra-ui/react";
 import { create as ipfsHttpClient, Options } from "ipfs-http-client";
 import artDAbi from "../abis/artDAbi.json";
 import { artDAddress } from "../constants/contracts";
-const ipfsApi: any = "https://gateway.pinata.cloud/ipfs/";
-const client = ipfsHttpClient(ipfsApi);
+import { Web3Storage } from "web3.storage";
+
+const key: any =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDNkNWIxODg4MGZCRDNlODFmMThjMTgwMTUxRUJhMzU0RERCNzk2MDQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjM3NzYwNTYzNTAsIm5hbWUiOiJSRUkifQ.MCkF8eYXgLAgMLVbLwOeabvEUrltjkXD5Vw81RCfsGY";
+const web3storage = new Web3Storage({ token: key });
 
 const CreateNft: NextPage = () => {
   const [mounted, setmounted] = useState<boolean>(false);
@@ -64,13 +67,20 @@ const CreateNft: NextPage = () => {
   });
 
   async function handleImage(e: any) {
-    const target = e.target;
-    const file = target.files[0];
     try {
-      const added = await client.add(file, {
-        progress: (prog) => console.log(`received: ${prog}`),
+      const target = e.target;
+      const file = target.files[0];
+      const name = "default";
+      const newfile = new File([file], name, {
+        type: file.type,
+        lastModified: file.lastModified,
       });
-      const url: string = `https://ipfs.infura.io/ipfs/${added.path}`;
+      const metadata = new File(
+        [JSON.stringify({ name: name })],
+        "metadata.json"
+      );
+      const path = await web3storage.put([newfile, metadata]);
+      const url: string = `https://ipfs.io/ipfs/${path}/${name}`;
       setFileUrl(url);
     } catch (error) {
       console.log("Error uploading file: ", error);
@@ -89,21 +99,23 @@ const CreateNft: NextPage = () => {
   async function createNft() {
     const { name, description, royalty } = formInput;
     if (!name || !description || !royalty || !fileUrl) return;
-    const data = JSON.stringify({
-      name,
-      description,
-      image: fileUrl,
-    });
+
     try {
-      const added = await client.add(data);
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      const nft = {
+        name,
+        description,
+        image: fileUrl,
+      };
+      const buffer = Buffer.from(JSON.stringify(nft));
+      const files = [new File([buffer], `metadata.json`)];
+      const cid = await web3storage.put(files);
+      const url = `${cid}/metadata.json`;
       createitem(url);
     } catch (error) {
       console.log("Error uploading file: ", error);
     }
   }
   async function createitem(url: string) {
-    console.log(isConnected);
     if (!isConnected) return;
     try {
       const tx = await ArtDcontract.mintNft(
